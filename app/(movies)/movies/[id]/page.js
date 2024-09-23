f28a6d7fc1f89a387c2ebffebf80195f8baa8f89
InSeop Hwang <inseop.hwang@hotmail.com>
2024-09-23 21:54:45 +0900

    update suspense function

--- app/(movies)/movies/[id]/page.js
+++ app/(movies)/movies/[id]/page.js
@@ -8,22 +8,16 @@ async function getMovie(id) {
     return response.json();
 }
 
-async function getVideos(id) {
-    console.log(`Fetching videos: ${Date.now()}`);
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    const response = await fetch(`${API_URL}/${id}/videos`);
-    return response.json();
-}
 
 export default async function MoveDetail({params: {id}}) {
-    console.log('==============');
-    console.log('start fetching');
+    // console.log('==============');
+    // console.log('start fetching');
     // [Serial sequence]
     // const movie = await getMovie(id);
     // const videos = await getVideos(id);
     
     // [Parallel sequence]
-    const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)])
-    console.log('end fetching');
+    const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+    // console.log('end fetching');
     return <h1> {movie.title} </h1>;
 }
